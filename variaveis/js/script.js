be2e617ce3d0daa9b79c5087c7d47aab827e6ce6
isaiasigashira19 <isaiaskassmofc@gmail.com>
2024-01-04 09:51:07 -0300

    aula de let var e const concluída

---
+++ variaveis/js/script.js
@@ -0,0 +1,38 @@
+var teste = 1;
+
+var nome = 'Isaias';
+
+console.log(teste);
+
+teste = 'Toyota';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'T1';
+var _nome = 'T2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome3 = 'T3';
+
+console.log(nome3);
+
+var meuPrimeiroVar = 'T4'; // camelCase
+
+console.log(meuPrimeiroVar);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+meuNome = 'Isaias';
+
+console.log(meuNome)
+
+
---
+++ variaveis/var-let-const/js/script2.js
@@ -0,0 +1,26 @@
+let nome = 'Isaias';
+const numero = 1;
+
+function dizerNome() {
+    var nome = 'Honda';
+    console.log(`O nome é ${nome}.`);
+}
+
+if(true) {
+    let nome = 'Toyota';
+    const numero = 2;
+    console.log(nome);
+    console.log(numero);
+}
+
+console.log(nome);
+console.log(numero);
+dizerNome();
+
+
+for(let i = 0; i < 3; i++) {
+    let nome = 'Anísio';
+    console.log(nome);
+    const numero = 3;
+    console.log(numero);
+}
