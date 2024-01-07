29e5bd77f24d2b38ebc25bd5ffd6140c238d004f
isaiasigashira19 <isaiaskassmofc@gmail.com>
2024-01-07 14:26:30 -0300

    tipo de dados atualizado

---
+++ tipos-d-dados/array/js/script.js
@@ -0,0 +1,17 @@
+var arr = [5, 'Isaias', true, {teste: 1, teste: 2}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+console.log(arr[0]);
+console.log(arr[3]);
+
+arr[4] = 10;
+arr[0] = 'modificado';
+
+console.log(arr);
+
+console.log(typeof arr);
